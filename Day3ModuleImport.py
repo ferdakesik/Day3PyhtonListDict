9d7c066ecb6029a7d172ff571cb50b2a8103a633
ferdakesik <ferdakesik@gmail.com>
2023-03-23 22:54:22 -0400

    updated

--- Day3ModuleImport.py
+++ Day3ModuleImport.py
@@ -1,2 +1,7 @@
-def hello():
-    print("welcome back")
+class house:
+    def __init__(self,width,length):
+        self.width=width
+        self.length=length
+        
+    def calculation(self):
+        return self.width*self.length
